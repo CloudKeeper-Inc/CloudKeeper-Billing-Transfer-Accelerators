530cb72050184437d8edacc10a5ca31970a94400
PriyanshPathak2002 <priyansh7072@gmail.com>
2025-04-14 12:44:31 +0530

    made changes in the utils file od the AWS-Cloudtrail for the s3 bucket policy depends on also made changes so that a cloud  trail without a  lms key can also be be replicated in alternate method

--- AWS-Cloudtrail/masterAccount/output.tf
+++ AWS-Cloudtrail/masterAccount/output.tf
@@ -1,21 +1,17 @@
 output "cloud_watch_logs_role_arn" {
   value = {for k, v in aws_iam_role.cloudtrail_logging_role: k => v.arn}
-  # value = aws_iam_role.cloudtrail_logging_role[0].arn
 }
 
 output "cloud_watch_logs_group_arn" {
   value = {for k, v in aws_cloudwatch_log_group.logs: k => v.arn}
-  # value = aws_cloudwatch_log_group.logs[0].arn
 }
 
 output "sns_topic_name" {
   value = {for k, v in aws_sns_topic.test: k => v.arn}
-  # value = aws_sns_topic.test[0].arn
 }
 
 output "kms_key_id" {
-  # value = {for k, v in aws_kms_key.kms_key: k => v.arn}
-  value = aws_kms_key.kms_key[0].arn
+  value = "${var.kms ? aws_kms_key.kms_key[0].arn : ""}"
 }
 
 output "s3_bucket_name" {
